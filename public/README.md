71c4632fb0d0d8d0f68470c56caae6e38de2c18d
Christopher Dirksen <chrisdirksen024@outlook.com>
2023-11-30 17:49:49 -0500

    commit

--- public/README.md
+++
@@ -1,4 +0,0 @@
-# faithOverFear
-Your Place For Sublimation And SVG Needs
-Faith Over Fear
-logo that says "Faith Over Fear", using light yellows and light blues
--- public/client/src/Components/serverless.js
+++
@@ -1,7 +0,0 @@
-const { MongoClient } = requuire('mongodb')
-
-exports.handler = async (e, context) => {
-    try{
-        const client = new MongoClient()
-    }
-}
